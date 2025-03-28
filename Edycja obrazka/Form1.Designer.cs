12a70c9c16b84883bf1fddd8344af18a45526af0
Natalia <natalia.kus@student.put.poznan.pl>
2025-03-28 13:32:24 +0100

    Kolorki

--- Edycja obrazka/Form1.Designer.cs
+++ Edycja obrazka/Form1.Designer.cs
@@ -59,65 +59,71 @@
             // 
             // btnRotate
             // 
+            this.btnRotate.BackColor = System.Drawing.Color.IndianRed;
             this.btnRotate.Location = new System.Drawing.Point(115, 110);
             this.btnRotate.Name = "btnRotate";
             this.btnRotate.Size = new System.Drawing.Size(84, 34);
             this.btnRotate.TabIndex = 2;
             this.btnRotate.Text = "Rotate";
-            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.UseVisualStyleBackColor = false;
             this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
             // 
             // rdo90
             // 
             this.rdo90.AutoSize = true;
+            this.rdo90.BackColor = System.Drawing.Color.IndianRed;
             this.rdo90.Location = new System.Drawing.Point(115, 22);
             this.rdo90.Name = "rdo90";
             this.rdo90.Size = new System.Drawing.Size(69, 24);
             this.rdo90.TabIndex = 3;
             this.rdo90.TabStop = true;
             this.rdo90.Text = "90 st";
-            this.rdo90.UseVisualStyleBackColor = true;
+            this.rdo90.UseVisualStyleBackColor = false;
             // 
             // rdo180
             // 
             this.rdo180.AutoSize = true;
+            this.rdo180.BackColor = System.Drawing.Color.IndianRed;
             this.rdo180.Location = new System.Drawing.Point(115, 50);
             this.rdo180.Name = "rdo180";
             this.rdo180.Size = new System.Drawing.Size(78, 24);
             this.rdo180.TabIndex = 4;
             this.rdo180.TabStop = true;
             this.rdo180.Text = "180 st";
-            this.rdo180.UseVisualStyleBackColor = true;
+            this.rdo180.UseVisualStyleBackColor = false;
             // 
             // rdo270
             // 
             this.rdo270.AutoSize = true;
+            this.rdo270.BackColor = System.Drawing.Color.IndianRed;
             this.rdo270.Location = new System.Drawing.Point(115, 80);
             this.rdo270.Name = "rdo270";
             this.rdo270.Size = new System.Drawing.Size(78, 24);
             this.rdo270.TabIndex = 5;
             this.rdo270.TabStop = true;
             this.rdo270.Text = "270 st";
-            this.rdo270.UseVisualStyleBackColor = true;
+            this.rdo270.UseVisualStyleBackColor = false;
             // 
             // btnInvert
             // 
+            this.btnInvert.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnInvert.Location = new System.Drawing.Point(81, 150);
             this.btnInvert.Name = "btnInvert";
             this.btnInvert.Size = new System.Drawing.Size(138, 33);
             this.btnInvert.TabIndex = 6;
             this.btnInvert.Text = "Invert Colors";
-            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.UseVisualStyleBackColor = false;
             this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
             // 
             // btnUpsideDown
             // 
+            this.btnUpsideDown.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnUpsideDown.Location = new System.Drawing.Point(81, 189);
             this.btnUpsideDown.Name = "btnUpsideDown";
             this.btnUpsideDown.Size = new System.Drawing.Size(138, 32);
             this.btnUpsideDown.TabIndex = 7;
             this.btnUpsideDown.Text = "Upside Down";
-            this.btnUpsideDown.UseVisualStyleBackColor = true;
+            this.btnUpsideDown.UseVisualStyleBackColor = false;
             this.btnUpsideDown.Click += new System.EventHandler(this.btnUpsideDown_Click);
             // 
             // Form1
